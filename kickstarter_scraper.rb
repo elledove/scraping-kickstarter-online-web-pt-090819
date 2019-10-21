894ce55a5ffc6351e4e152dd2c872b59d31c4fc0
Lauren Dove <elledoveinc@gmail.com>
2019-10-21 23:49:02 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,18 @@
-# require libraries/modules here
+require 'nokogiri'
+require 'pry'
+ #require libraries/modules here
 
 def create_project_hash
-  # write your code here
+  
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
+#create_project_hash
+
+
+
+
+#projects: kickstarter.css("li.project.grid_4").first
+#title: project.css("h2.bbcard_name strong a").text
+#image link: project.css("div.project-thumbnail a img").attribute("src").value
